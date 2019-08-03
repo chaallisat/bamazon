b38872d7bb8c2e14e6123328aa345c92db57bd49
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-08-03 12:17:39 -0700

    created connection for node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,6 +18,16 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  end.connection();
+  afterConnection();
   
 });
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+
+      for (let i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+      }
+      connection.end();
+    });
+  }
