9b9a9f18b669b3fdfd258db05d7e4264f54c8742
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-08-03 10:28:41 -0700

    initial files

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Starry Sky", "Home Decor", 50000, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mona Lisa", "Home Decor", 999999999, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fluffy", "Toy", 19.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Doll", "Toy", 3.49, 200);
